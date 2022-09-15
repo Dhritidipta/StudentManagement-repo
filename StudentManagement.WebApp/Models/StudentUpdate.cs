0c5e07e46042d6b65e8035acac09901d90af0bee
Dhritidipta <61513692+Dhritidipta@users.noreply.github.com>
2022-09-15 19:48:15 +0530

    Add Delete and Update functions

---
+++ StudentManagement.WebApp/Models/StudentUpdate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.WebApp.Models
+{
+    public class StudentUpdate
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public char Section { get; set; }
+        public string Gender { get; set; }
+        public int Fees { get; set; }
+        public int Age { get; set; }
+        public string CourseName { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DateOfAdmission { get; set; }
+    }
+}
